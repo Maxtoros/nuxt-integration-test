a39d0113af5ee6a7b7c7f7281b0243eaaa37d0c0
Maxtoros <ndm.khammeri@gmail.com>
2025-10-04 20:51:21 +0200

    chore(fonts): add Larken font, define font faces and nuxt/fonts configuration
    Related to JIRA-003

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image'],
+  css: ['~/assets/styles/index.css'],
+  fonts: {
+    defaults: {
+      weights: [400],
+      styles: ['normal', 'italic'],
+    },
+    families: [
+      { name: 'Larken', provider: 'none' },
+    ]
+  }
 })
